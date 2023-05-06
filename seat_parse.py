127e51955164a7f14c8b7783094c0c078eb21723
Szabolcs Antal Dobra <szabolcs.antal.dobra@ericsson.com>
2023-05-07 00:09:51 +0200

    New folders. Removed unused code.

--- seat_parse.py
+++ seat_parse.py
@@ -8,12 +8,6 @@ with open(input_file, "r", encoding="utf-8") as f:
 
 matches = re.findall(r"<p class=\"sc-pcJja hDGzhX\"><span>(\d+)<\/span><span>(.*?)<\/span>", html_code)
 
-# seat_status = {f"Seat {int(id):04d}": status.strip() for id, status in matches if status.strip()}
-
-# with open(output_file, "w") as f:
-    # for seat, status in seat_status.items():
-        # f.write(f"{seat} is {status}\n")
-
 # Convert matches to a list of strings
 matches = [(m[0], re.sub(r"<div.*?>|<\/div>", "", m[1])) for m in matches]
 
